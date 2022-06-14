9ae15d22961dc10dce7d3dd15788f0a31c8f6f5e
EricHu <boqinhu1207@gmail.com>
2022-06-14 17:05:09 +1000

    Init

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var myAge = 28
+
+var iLikeCheese = true
+
+console.log("My age is" + myAge);
+
+console.log("I like cheese" + iLikeCheese);
+
+//myAge++;
+//myAge = 40
+var iLikeCheese = false
+
+console.log("My age next year will be" + myAge);
+console.log("I like cheese" + iLikeCheese);
+
+var myAge = 40
